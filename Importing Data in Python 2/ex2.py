1d1ad0c13f5a3c781b875e557fc4a0d7bdb8ee9d
Rakshith Vasudev <rakshithvbharani@gmail.com>
2018-08-25 17:03:08 -0700

    added ex 2

--- Importing Data in Python 2/ex2.py
+++ Importing Data in Python 2/ex2.py
@@ -13,3 +13,18 @@ Execute the rest of the code to plot histogram of the first feature in the DataF
 
 """
 
+# Import package
+from urllib.request import urlretrieve
+
+# Import pandas
+import pandas as pd
+
+# Assign url of file: url
+url = 'https://s3.amazonaws.com/assets.datacamp.com/production/course_1606/datasets/winequality-red.csv'
+
+# Save file locally
+urlretrieve(url,"winequality-red.csv")
+
+# Read file into a DataFrame and print its head
+df = pd.read_csv('winequality-red.csv', sep=';')
+print(df.head())
