030c8d597478d187d7c0bcdaad5a2c0aa744e4d1
Rakshith Vasudev <rakshithvbharani@gmail.com>
2018-10-31 12:20:31 -0700

    add ex3,4

--- Intro to pyspark/ex3.py
+++ Intro to pyspark/ex3.py
@@ -12,3 +12,9 @@ Call print() on sc to verify there's a SparkContext in your environment.
 print() sc.version to see what version of Spark is running on your cluster.
 Take Hint (-30 XP)
 """
+
+# Verify SparkContext
+print(sc)
+
+# Print Spark version
+print(sc.version)
