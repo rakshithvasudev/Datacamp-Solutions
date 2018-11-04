ea0190e8e34e7e604c2cec0ec7e541f09f6f3135
Rakshith Vasudev <rakshithvbharani@gmail.com>
2018-11-04 15:38:06 -0600

    add new entry

---
+++ Intro to SQL for Data science/ex5.sql
@@ -0,0 +1,7 @@
+/*
+Onboarding | Bullet Exercises
+Another new feature we're introducing is the bullet exercise, which allows you to easily practice a new concept through repetition. Check it out below!
+*/
+
+SELECT 'SQL is cool!'
+AS result;
