bfc7b8062c16098dc2ebf3379e88128063ad8916
Rakshith Vasudev <rakshithvbharani@gmail.com>
2018-11-24 09:46:43 -0800

    ADD EX2 and 3

---
+++ Intro to SQL for Data science/aggregate-functions/ex2.sql
@@ -0,0 +1,11 @@
+SELECT SUM(gross)
+FROM films;
+
+SELECT AVG(gross)
+FROM films;
+
+SELECT MIN(gross)
+FROM films;
+
+SELECT MAX(gross)
+FROM films;
---
+++ Intro to SQL for Data science/aggregate-functions/ex3.sql
@@ -0,0 +1,15 @@
+SELECT SUM(gross)
+FROM films
+WHERE release_year >= 2000;
+
+SELECT AVG(gross)
+FROM films
+WHERE title LIKE 'A%';
+
+SELECT MIN(gross)
+FROM films
+WHERE release_year = 1994;
+
+SELECT MAX(gross)
+FROM films
+WHERE release_year BETWEEN 1994 and 2012;
