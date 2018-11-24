b6910ebd0293acdddd93504ba08b90468e355cbb
Rakshith Vasudev <rakshithvbharani@gmail.com>
2018-11-24 10:06:40 -0800

    add difference

---
+++ Intro to SQL for Data science/aggregate-functions/ex6.sql
@@ -0,0 +1,9 @@
+-- get the count(deathdate) and multiply by 100.0
+-- then divide by count(*)
+
+SELECT  (COUNT(deathdate)*100.0/COUNT(*)) AS percentage_dead
+FROM people;
+
+
+SELECT MAX(release_year)-MIN(release_year) AS difference
+FROM films;
