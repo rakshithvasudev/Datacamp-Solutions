1343f0352e305761bb81f0cb9564a88dfef2ba72
Rakshith Vasudev <rakshithvbharani@gmail.com>
2018-12-17 16:57:26 -0800

    added

---
+++ A-B Testing/ex1.py
@@ -0,0 +1,9 @@
+"""Understanding the key components of an A/B test
+From our discussion so far, which of the following do you think is the key component of an A/B test's usefulness?
+
+
+"""
+# Having the users be randomly assigned to groups.
+# Correct! Randomness helps ensure nothing else is impacting our observed results.
+
+
---
+++ A-B Testing/ex2.py
@@ -0,0 +1,9 @@
+"""
+Defining meaningful KPIs
+Identify which of the following would not be a meaningful KPI for a small foodtruck trying to optimize its business.
+"""
+
+# Meals sold over the past year.
+
+# Correct! For some businesses yearly metrics are useful. But for a food truck trying to optimize its sales this is too long of a period.
+
