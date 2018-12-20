ce1986154a3c09b7e638f2a5d932f7c9b6fcdb34
Rakshith Vasudev <rakshithvbharani@gmail.com>
2018-12-20 11:14:53 -0800

    added interactive images

--- Intro to pyspark/ex9.py
+++ Intro to pyspark/ex9.py
@@ -29,3 +29,18 @@ Register spark_temp as a temporary table named "temp" using the .createOrReplace
 Examine the list of tables again!
 Take Hint (-30 XP)
 """
+
+# Create pd_temp
+pd_temp = pd.DataFrame(np.random.random(10))
+
+# Create spark_temp from pd_temp
+spark_temp = spark.createDataFrame(pd_temp)
+
+# Examine the tables in the catalog
+print(spark.catalog.listTables())
+
+# Add spark_temp to the catalog
+spark_temp.createOrReplaceTempView("temp")
+
+# Examine the tables in the catalog again
+print(spark.catalog.listTables())
