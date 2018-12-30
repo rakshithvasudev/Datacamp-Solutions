2a0a151618c82a2d99c42261fa7a78c9826dbdba
Rakshith Vasudev <rakshithvbharani@gmail.com>
2018-12-29 20:12:51 -0800

    added ex

---
+++ Recommendation Engines in Pyspark/How ALS works/ex2.py
@@ -0,0 +1,25 @@
+"""
+Matrix Multiplication
+To understand matrix multiplication more directly, let's do some matrix operations manually.
+
+Instructions
+100 XP
+Matrices a and b are Pandas dataframes. Use the .head() method on each of them to view them.
+Work out the product of these two matrices on your own.
+Enter the values of the product of these two matrices into the product array
+Use the validation on the last line of code to evaluate your estimate. The .dot() method multiplies two matrices together.
+Take Hint (-30 XP)
+"""
+
+# Use the .head() method to view the contents of matrices a and b
+print("Matrix A: ")
+print (a.head())
+
+print("Matrix B: ")
+print (b.head())
+
+# Complete the matrix called "product" with the product of matrices a and b.
+product = np.array([[10,12], [15,18]])
+
+# Run this validation to see how your estimate performs
+product == np.dot(a,b)
---
+++ Recommendation Engines in Pyspark/ex3.py
@@ -0,0 +1,7 @@
+"""
+Collaborative vs Content-Based Filtering
+Below are statements that are often used when providing recommendations, select the one that DOES NOT indicate collaborative filtering.
+"""
+# Because you liked that product, we think you'll like this product"
+
+
